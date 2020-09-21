637ed9eb8ffa4575cf5a85d97296645a6eab63f9
ahmed-elgendy <65793731+ahmed-elgendy@users.noreply.github.com>
2020-09-21 01:17:36 -0700

    first commit

---
+++ js/custom-en.js
@@ -0,0 +1,110 @@
+(function ($) {
+    // Navigation scrolls
+    $(".button-collapse").sideNav();
+
+
+})(jQuery);
+
+
+$('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        hover: true, // Activate on hover
+        belowOrigin: true, // Displays dropdown below the button
+        alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    }
+);
+
+
+//  strat counter project
+$.fn.jQuerySimpleCounter = function (options) {
+    var settings = $.extend({
+        start: 0,
+        end: 100,
+        easing: 'swing',
+        duration: 400,
+        complete: ''
+    }, options);
+
+    var thisElement = $(this);
+
+    $({count: settings.start}).animate({count: settings.end}, {
+        duration: settings.duration,
+        easing: settings.easing,
+        step: function () {
+            var mathCount = Math.ceil(this.count);
+            thisElement.text(mathCount);
+        },
+        complete: settings.complete
+    });
+};
+
+
+$('#number1').jQuerySimpleCounter({end: 12, duration: 3000});
+$('#number2').jQuerySimpleCounter({end: 55, duration: 3000});
+$('#number3').jQuerySimpleCounter({end: 359, duration: 2000});
+$('#number4').jQuerySimpleCounter({end: 246, duration: 2500});
+
+
+/* AUTHOR LINK */
+$('.about-me-img').hover(function () {
+    $('.authorWindowWrapper').stop().fadeIn('fast').find('p').addClass('trans');
+}, function () {
+    $('.authorWindowWrapper').stop().fadeOut('fast').find('p').removeClass('trans');
+});
+//  end  counter project
+
+
+
+
+
+/* Whatsapp Chat Widget by www.bloggermix.com */
+$(document).on("click", "#send-it", function() {
+  var a = document.getElementById("chat-input");
+  if ("" != a.value) {
+    var b = $("#get-number").text(),
+      c = document.getElementById("chat-input").value,
+      d = "https://web.whatsapp.com/send",
+      e = b,
+      f = "&text=" + c;
+    if (
+      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+    )
+      var d = "whatsapp://send";
+    var g = d + "?phone=+201012370014" + e + f;
+    window.open(g, "_blank");
+  }
+}),
+  $(document).on("click", ".informasi", function() {
+    (document.getElementById("get-number").innerHTML = $(this)
+      .children(".my-number")
+      .text()),
+      $(".start-chat,.get-new")
+        .addClass("show")
+        .removeClass("hide"),
+      $(".home-chat,.head-home")
+        .addClass("hide")
+        .removeClass("show"),
+      (document.getElementById("get-nama").innerHTML = $(this)
+        .children(".info-chat")
+        .children(".chat-nama")
+        .text()),
+      (document.getElementById("get-label").innerHTML = $(this)
+        .children(".info-chat")
+        .children(".chat-label")
+        .text());
+  }),
+  $(document).on("click", ".close-chat", function() {
+    $("#whatsapp-chat")
+      .addClass("hide")
+      .removeClass("show");
+  }),
+  $(document).on("click", ".blantershow-chat", function() {
+    $("#whatsapp-chat")
+      .addClass("show")
+      .removeClass("hide");
+  });
+
+    // massenger chat 
